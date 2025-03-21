0c0a446b2cf02fc7a0e4c5bbe6221f2ba535facc
henri <henri@vanbml.nl>
2025-03-21 07:57:42 +0100

    update debug string

--- hardware/Rtl433.cpp
+++ hardware/Rtl433.cpp
@@ -370,7 +370,7 @@ bool CRtl433::ParseData(std::map<std::string, std::string>& data)
 		if (FindField(data, "cmd")) {
 			uint8_t cmd = atoi(data["cmd"].c_str());
 			bool bOn = (cmd == 14);
-			Log(LOG_STATUS, "[Rtl433] Generic-Remote - cmd(%d) bOn(%d)) sinsoridx(%d)", cmd, bOn, sensoridx);
+			Log(LOG_STATUS, "[Rtl433] Generic-Remote - cmd(%d) bOn(%d)", cmd, bOn);
 		}
 
 		Log(LOG_STATUS, "[Rtl433] Generic-Remote");
